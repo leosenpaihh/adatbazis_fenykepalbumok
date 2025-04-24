773703d690d29737609a7e3c98a75cc76891799f
boroczi <boroczi2003@gmail.com>
2025-04-24 16:04:11 +0200

    added location deletion and update

--- controllers/location_handler.php
+++ controllers/location_handler.php
@@ -7,6 +7,24 @@ if (!isset($_SESSION['felhasznalo'])) {
     exit;
 }
 
+if (isset($_POST['torles']) && empty($hibak)) {
+     $sql = "DELETE FROM TELEPULES WHERE ID = :id";
+     $stid = oci_parse($conn, $sql);
+     oci_bind_by_name($stid, ':id', $_POST['id']);
+     if (!oci_execute($stid)) {
+         $error = oci_error($stid);
+         $_SESSION['hiba'] = "Hiba történt a helyszín törlése során: " . $error['message'];
+         header("Location: ../pages/location.php");
+         exit;
+     }
+
+     oci_commit($conn);
+
+     $_SESSION['message'] = "A helyszín sikeresen törölve!";
+     header("Location: ../pages/location.php");
+     exit;
+}
+
 $new_orszag = $_POST['orszag'];
 $new_megye = $_POST['megye'];
 $new_telepules = $_POST['telepules'];
@@ -36,20 +54,40 @@ if (empty($hibak)) {
 }
 
 if (empty($hibak)) {
-    $sql = "INSERT INTO TELEPULES (ORSZAG, MEGYE, TELEPULES) VALUES (:orszag, :megye, :telepules)";
-    $stid = oci_parse($conn, $sql);
-    oci_bind_by_name($stid, ':orszag', $new_orszag);
-    oci_bind_by_name($stid, ':megye', $new_megye);
-    oci_bind_by_name($stid, ':telepules', $new_telepules);
+    if (isset($_POST['letrehozas'])) {
+        $sql = "INSERT INTO TELEPULES (ORSZAG, MEGYE, TELEPULES) VALUES (:orszag, :megye, :telepules)";
+        $stid = oci_parse($conn, $sql);
+        oci_bind_by_name($stid, ':orszag', $new_orszag);
+        oci_bind_by_name($stid, ':megye', $new_megye);
+        oci_bind_by_name($stid, ':telepules', $new_telepules);
 
-    if (oci_execute($stid)) {
-        $_SESSION['message'] = "A helyszín sikeresen hozzáadva!";
-        header("Location: ../pages/location.php");
-        exit;
-    } else {
-        $_SESSION['hiba'] = "Hiba történt a helyszín hozzáadása során!";
-        header("Location: ../pages/location.php");
-        exit;
+        if (oci_execute($stid)) {
+            $_SESSION['message'] = "A helyszín sikeresen hozzáadva!";
+            header("Location: ../pages/location.php");
+            exit;
+        } else {
+            $_SESSION['hiba'] = "Hiba történt a helyszín hozzáadása során!";
+            header("Location: ../pages/location.php");
+            exit;
+        }
+    } elseif (isset($_POST['modositas'])) {
+        $sql = "UPDATE TELEPULES SET ORSZAG = :orszag, MEGYE = :megye, TELEPULES = :telepules WHERE ID = :id";
+        $stid = oci_parse($conn, $sql);
+        oci_bind_by_name($stid, ':orszag', $new_orszag);
+        oci_bind_by_name($stid, ':megye', $new_megye);
+        oci_bind_by_name($stid, ':telepules', $new_telepules);
+        oci_bind_by_name($stid, ':id', $_POST['id']);
+        echo $new_orszag . $new_megye . $new_telepules . $_POST['id'];
+
+        if (oci_execute($stid)) {
+            $_SESSION['message'] = "A helyszín sikeresen frissítve!";
+            header("Location: ../pages/location.php");
+            exit;
+        } else {
+            $_SESSION['hiba'] = "Hiba történt a helyszín frissítése során!";
+            header("Location: ../pages/location.php");
+            exit;
+        }
     }
 } else {
     $_SESSION['hiba'] = implode("<br>", $hibak);
--- pages/location.php
+++ pages/location.php
@@ -17,6 +17,14 @@ if (isset($_SESSION['message'])) {
     echo "<p>" . $_SESSION['message'] . "</p>";
     unset($_SESSION['message']);
 }
+
+$telepulesek = [];
+
+$stid = oci_parse($conn, "SELECT * FROM TELEPULES");
+oci_execute($stid);
+while ($row = oci_fetch_assoc($stid)) {
+    $telepulesek[] = $row;
+}
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +32,7 @@ if (isset($_SESSION['message'])) {
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <script type="text/javascript" src="shared/modify.js"></script>
     <title>Település hozzáadása</title>
 </head>
 <body>
@@ -39,7 +48,68 @@ if (isset($_SESSION['message'])) {
     <label for="telepules">Település:</label><br>
     <input type="text" id="telepules" name="telepules" required maxlength="100"><br>
 
-    <input type="submit" value="Helyszín hozzáadása">
+    <input type="submit" name="letrehozas" value="Helyszín hozzáadása">
 </form>
+<h2>Helyszínek</h2>
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Ország</th>
+            <th>Megye</th>
+            <th>Település</th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php if (count($telepulesek) > 0): ?>
+            <?php foreach ($telepulesek as $telepules): ?>
+                <tr>
+                    <td><?= htmlspecialchars($telepules['ID']) ?></td>
+                    <td><?= htmlspecialchars($telepules['ORSZAG']) ?></td>
+                    <td><?= htmlspecialchars($telepules['MEGYE']) ?></td>
+                    <td><?= htmlspecialchars($telepules['TELEPULES']) ?></td>
+                    <td>
+                        <button onclick='modify(
+                            <?= json_encode($telepules) ?>,
+                            "modositas_id", "eredeti_id", "modositas_orszag", "eredeti_orszag", "modositas_megye", "eredeti_megye", "modositas_telepules", "eredeti_telepules"
+                        )'>
+                            Módosítás
+                        </button>
+                    </td>
+                    <td>
+                        <form action="../controllers/location_handler.php" method="post">
+                            <input type="hidden" id="id" name="id" value='<?= $telepules["ID"] ?>'>
+
+                            <input type="submit" name="torles" value="Törlés">
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <tr>
+                <td colspan="3">Nincsenek helyszínek.</td>
+            </tr>
+        <?php endif; ?>
+    </tbody>
+</table>
+<hr />
+<div id="modositas_form" style="display: none">
+<form action="../controllers/location_handler.php" method="post">
+    <label for="nev">Módosítsd a települést:</label><br>
+    <input type="text" id="modositas_id" name="id" required maxlength="100">
+    <input type="hidden" id="eredeti_id" name="eredeti_id" value="">
+    <input type="text" id="modositas_orszag" name="orszag" required maxlength="100">
+    <input type="hidden" id="eredeti_orszag" name="eredeti_orszag" value="">
+    <input type="text" id="modositas_megye" name="megye" required maxlength="100">
+    <input type="hidden" id="eredeti_megye" name="eredeti_megye" value="">
+    <input type="text" id="modositas_telepules" name="telepules" required maxlength="100">
+    <input type="hidden" id="eredeti_telepules" name="eredeti_telepules" value="">
+
+    <input type="submit" name="modositas" value="Módosítás mentése">
+</form>
+<button name="megse" onclick="closeModificationForm()">Mégse</button>
+</div>
 </body>
 </html>

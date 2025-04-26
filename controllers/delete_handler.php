cc7b315c9f83bfd636ca4c91bfcb418375cd8309
Leo <lakatosleonardo1123@gmail.com>
2025-04-26 13:51:48 +0200

    Updating the main site and additional changes

---
+++ controllers/delete_handler.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../includes/db.php';
+session_start();
+
+// Ellenőrizzük, hogy POST kérés és van-e kép ID
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['kep_id'])) {
+    $kepId = (int)$_POST['kep_id'];
+
+    // Először ellenőrizzük, hogy a bejelentkezett user töltheti-e
+    $query = "SELECT FELHASZNALO_FELHASZNALONEV FROM Kep WHERE ID = :id";
+    $stid = oci_parse($conn, $query);
+    oci_bind_by_name($stid, ':id', $kepId);
+    oci_execute($stid);
+    $row = oci_fetch_assoc($stid);
+
+    if (!$row) {
+        echo "A kép nem található.";
+        exit;
+    }
+
+    if ($row['FELHASZNALO_FELHASZNALONEV'] !== $_SESSION['felhasznalo']['felhasznalonev']) {
+        echo "Nincs jogosultságod törölni ezt a képet.";
+        exit;
+    }
+
+    // Jogosultság rendben, töröljük a képet
+    $deleteQuery = "DELETE FROM Kep WHERE ID = :id";
+    $deleteStid = oci_parse($conn, $deleteQuery);
+    oci_bind_by_name($deleteStid, ':id', $kepId);
+    oci_execute($deleteStid);
+
+    // Sikeres törlés után visszairányítunk
+    header("Location: ../index.php");
+    exit;
+} else {
+    echo "Hibás kérés.";
+}
+?>

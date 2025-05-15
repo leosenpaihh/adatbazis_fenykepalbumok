c6b03fa5fd23ac915f66a8cd19be5ae703ac0dc7
leosenpaihh <lakatosleonardo1123@gmail.com>
2025-05-15 16:39:15 +0200

    Fixing and replacing the trigger, adding number type array for comment id, making the site more compatible desktop/mobile

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,8 @@
 <?php
 return [
-    'host' => 'adb.eu-stockholm-1.oraclecloud.com',  // Az Autonomous DB hostja
-    'port' => '1522',  // Az Oracle Autonomous DB portja (általában 1522)
-    'username' => 'ADMIN',
-    'password' => 'Fenykepalbumok123',
-    'db_name' => 'g3d0599c9f52bff_bocdvdqnn2nauu2k_high.adb.oraclecloud.com',  // Az adatbázis neve
-    'wallet' => getenv('WALLET_PATH'),  // A környezeti változó használata
-
+    'host' => 'localhost',
+    'port' => '1521',
+    'sid' => 'xe',
+    'username' => 'system',
+    'password' => 'oracle',
 ];
--- includes/db.php
+++ includes/db.php
@@ -1,26 +1,18 @@
 <?php
 $config = include 'config.php';
 
-// Wallet mappa beállítása
-putenv("TNS_ADMIN=" . $config['wallet']);
-
 $tns = "
 (DESCRIPTION =
     (ADDRESS_LIST =
-        (ADDRESS = (PROTOCOL = TCPS)(HOST = {$config['host']})(PORT = {$config['port']}))
+        (ADDRESS = (PROTOCOL = TCP)(HOST = {$config['host']})(PORT = {$config['port']}))
     )
     (CONNECT_DATA =
-        (SERVICE_NAME = {$config['db_name']})
-    )
-    (SECURITY =
-        (SSL_SERVER_DN_MATCH = yes)
+        (SID = {$config['sid']})
     )
 )";
 
 $conn = oci_connect($config['username'], $config['password'], $tns, 'UTF8');
 
 if (!$conn) {
-    $e = oci_error();
-    die("Kapcsolódás hiba: " . $e['message']);
+    die('Adatbázis kapcsolat hiba.');
 }
-?>
